711f9358ee8eca4ff0461f8d171a6b70ce38dd20
Hengkoro20 <155617022+Hengkoro20@users.noreply.github.com>
2024-12-25 17:39:41 +0700

    Add files via upload

---
+++ saklar.js
@@ -0,0 +1,159 @@
+function saklar() {
+  let toggle1 = document.getElementById("default-toggle1");
+  let toggle2 = document.getElementById("default-toggle2");
+  let toggle3 = document.getElementById("default-toggle3");
+  let toggle4 = document.getElementById("default-toggle4");
+  let toggle5 = document.getElementById("default-toggle5");
+  let toggle6 = document.getElementById("default-toggle6");
+  let toggle7 = document.getElementById("default-toggle7");
+  let toggle8 = document.getElementById("default-toggle8");
+  let toggle9 = document.getElementById("default-toggle9");
+  let toggle10 = document.getElementById("default-toggle10");
+
+
+  let lampu1 = document.getElementById("lampu1");
+  let lampu2 = document.getElementById("lampu2");
+  let lampu3 = document.getElementById("lampu3");
+  let lampu4 = document.getElementById("lampu4");
+  let lampu5 = document.getElementById("lampu5");
+  let lampu6 = document.getElementById("lampu6");
+  let lampu7 = document.getElementById("lampu7");
+  let lampu8 = document.getElementById("lampu8");
+  let lampu9 = document.getElementById("lampu9");
+  let lampu10 = document.getElementById("lampu10");
+
+
+  if (toggle1.checked) {
+    lampu1.src = "assets/images/on.gif";
+  } else {
+    lampu1.src = "assets/images/off.gif";
+  }
+
+  if (toggle2.checked) {
+    lampu2.src = "assets/images/on.gif";
+  } else {
+    lampu2.src = "assets/images/off.gif";
+  }
+
+  if (toggle3.checked) {
+    lampu3.src = "assets/images/on.gif";
+  } else {
+    lampu3.src = "assets/images/off.gif";
+  }
+
+  if (toggle4.checked) {
+    lampu4.src = "assets/images/on.gif";
+  } else {
+    lampu4.src = "assets/images/off.gif";
+  }
+
+  if (toggle5.checked) {
+    lampu5.src = "assets/images/on.gif";
+  } else {
+    lampu5.src = "assets/images/off.gif";
+  }
+
+  if (toggle6.checked) {
+    lampu6.src = "assets/images/on.gif";
+  } else {
+    lampu6.src = "assets/images/off.gif";
+  }
+
+  if (toggle7.checked) {
+    lampu7.src = "assets/images/on.gif";
+  } else {
+    lampu7.src = "assets/images/off.gif";
+  }
+
+  if (toggle8.checked) {
+    lampu8.src = "assets/images/on.gif";
+  } else {
+    lampu8.src = "assets/images/off.gif";
+  }
+
+  if (toggle9.checked) {
+    lampu9.src = "assets/images/on.gif";
+  } else {
+    lampu9.src = "assets/images/off.gif";
+  }
+
+  if (toggle10.checked) {
+    lampu10.src = "assets/images/on.gif";
+  } else {
+    lampu10.src = "assets/images/off.gif";
+  }
+
+
+}
+
+function saklar2() {
+  let toggleR = document.getElementById("default-toggleR2");
+  let lampuR = document.getElementById("lampuR2");
+
+  if (toggleR.checked) {
+    lampuR.src = "assets/images/on.gif";
+  } else {
+    lampuR.src = "assets/images/off.gif";
+  }
+}
+
+function saklar3() {
+  let toggleR3 = document.getElementById("default-toggleR3");
+  let toggleRR3 = document.getElementById("default-toggleRR3");
+  let lampuR3 = document.getElementById("lampuR3");
+  let lampuRR3 = document.getElementById("lampuRR3");
+
+  if (toggleR3.checked) {
+    lampuR3.src = "assets/images/on.gif";
+  } else {
+    lampuR3.src = "assets/images/off.gif";
+  }
+
+  if (toggleRR3.checked) {
+    lampuRR3.src = "assets/images/on.gif";
+  } else {
+    lampuRR3.src = "assets/images/off.gif";
+  }
+}
+
+function saklar4() {
+  let toggleR4 = document.getElementById("default-toggleR4");
+  let toggleRR4 = document.getElementById("default-toggleRR4");
+  let toggleRRR4 = document.getElementById("default-toggleRRR4");
+  let toggleRRRR4 = document.getElementById("default-toggleRRRR4");
+  let lampuR4 = document.getElementById("lampuR4");
+  let lampuRR4 = document.getElementById("lampuRR4");
+  let lampuRRR4 = document.getElementById("lampuRRR4");
+  let lampuRRRR4 = document.getElementById("lampuRRRR4");
+
+  if (toggleR4.checked) {
+    lampuR4.src = "assets/images/on.gif";
+  } else {
+    lampuR4.src = "assets/images/off.gif";
+  }
+
+  if (toggleRR4.checked) {
+    lampuRR4.src = "assets/images/on.gif";
+  } else {
+    lampuRR4.src = "assets/images/off.gif";
+  }
+
+  if (toggleRR4.checked) {
+    lampuRR4.src = "assets/images/on.gif";
+  } else {
+    lampuRR4.src = "assets/images/off.gif";
+  }
+
+  if (toggleRRR4.checked) {
+    lampuRRR4.src = "assets/images/on.gif";
+  } else {
+    lampuRRR4.src = "assets/images/off.gif";
+  }
+
+  if (toggleRRRR4.checked) {
+    lampuRRRR4.src = "assets/images/on.gif";
+  } else {
+    lampuRRRR4.src = "assets/images/off.gif";
+  }
+}
+
